222aae7a22ced059b1847ccf3904c097d17b8f45
aranallo121 <aranallo121@gmail.com>
2016-08-17 11:02:20 -0600

    updated project to include function listBikes

---
+++ social.js
@@ -0,0 +1,58 @@
+var cyclist = {
+    name:"Anthony",
+    age: 29,
+    bikes: [
+        {
+            model:"ReignX",
+            brand:"Giant",
+            uses: ["downhill", 
+                   "free ride", 
+                   "park"], 
+            isFavorite: false
+        }, 
+        {
+            model:"Trance",
+            brand:"Giant", 
+            uses: ["all mountain", 
+                   "resort",
+                  "trail"], 
+            isFavorite:false
+        }, 
+        {
+            model: "SV650",
+            brand: "Suzuki", 
+            uses: ["road", 
+                   "canyons", 
+                   "speed"],
+            isFavorite:false
+        }, 
+        {
+            model:"GS850",
+            brand: "Suzuki", 
+            uses: ["looks", 
+                   "commuting", 
+                   "project"], 
+            isFavorite:true
+        }
+    ],
+    favoriteTrails:["Procupine Rim",
+                    "Rush", 
+                    "Sap Tree",
+                    "911",
+                    "Jack Rabbit",
+                    "Cave Trail"],
+    gear:["gloves",
+          "helmet",
+          "jersey",
+          "shorts",
+          "goggles"],
+    listBikes:function() {
+        for (i=0; i<bikes.length; i++){
+            console.log(bikes[i]);
+        }
+  }
+  
+    
+}
+
+  
